a3c1ef9b8193c00e9ddfa3948b19117be3962487
fillopfy <abhi456tm@gmail.com>
2020-07-18 16:24:15 +0530

    Updates

---
+++ precache-manifest.b7f751269717219f2eba438ac67142a3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "789a598b77c45b9c79e20a2b105e644c",
+    "url": "/tictactoe/index.html"
+  },
+  {
+    "revision": "030be956a409d02e1708",
+    "url": "/tictactoe/static/css/main.c74d7992.chunk.css"
+  },
+  {
+    "revision": "a2436778992cd372315b",
+    "url": "/tictactoe/static/js/2.b9926c31.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tictactoe/static/js/2.b9926c31.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "030be956a409d02e1708",
+    "url": "/tictactoe/static/js/main.2830f489.chunk.js"
+  },
+  {
+    "revision": "4434fef27fe19069e69b",
+    "url": "/tictactoe/static/js/runtime-main.666fab5a.js"
+  }
+]);
